a776948683b2f68ffa75772615791534060514c0
sofi614 <alvarengasofiajaqueline@gmail.com>
2026-09-09 15:47:14 -0300

    feat: completar actualizacion y eliminacion de etiquetas

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createTag, allTags } from "../controllers/tag.controller.js";
-import { createTagValidation } from "../validators/tag.validator.js";
+import { createTag, allTags, updateTag, deleteTag } from "../controllers/tag.controller.js";
+import { createTagValidation, updateTagValidation, tagIdValidation } from "../validators/tag.validator.js";
 import { handleValidationErrors } from "../middlewares/validation.middleware.js";
 
 const router = Router();
 
 router.post("/tags", createTagValidation, handleValidationErrors, createTag);
 router.get("/tags", allTags);
+router.put("/tags/:id", updateTagValidation, handleValidationErrors, updateTag);
+router.delete("/tags/:id", tagIdValidation, handleValidationErrors, deleteTag);
 
 export default router;
